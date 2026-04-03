b6f6e85ce22382c639ca72b4a44d0b3a760e506b
koki <y.kouki2004@icloud.com>
2026-04-03 15:38:31 +0900

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by 吉岡晃基　 on 2026/04/03.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
